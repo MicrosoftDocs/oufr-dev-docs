c26e9020b8e2693e08521f569e9d011210e04b24
VitalieBraga <vibraga@microsoft.com>
2019-05-14 14:56:22 -0700

    Some more changes to fix some errors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,126 @@
 import { IYamlTocItem } from '@microsoft/api-documenter/lib/yaml/IYamlTocFile';
-import { categories, ICategory } from '@uifabric/fabric-website/lib/SiteDefinition/SiteDefinition.pages/Controls/web';
-import fs = require('fs');
+import {JsonFile} from '@microsoft/node-core-library';
+
+export const categories = {
+    'Basic Inputs': {
+      Button: {},
+      Checkbox: {},
+      ChoiceGroup: {},
+      ComboBox: {},
+      Dropdown: {},
+      Label: {},
+      Link: {},
+      Rating: {},
+      SearchBox: {},
+      Slider: {},
+      SpinButton: {},
+      TextField: {},
+      Toggle: {}
+    },
+    'Galleries & Pickers': {
+      Pickers: {},
+      Calendar: {},
+      ColorPicker: {},
+      DatePicker: {},
+      PeoplePicker: {},
+      SwatchColorPicker: {}
+    },
+    'Items & Lists': {
+      List: { title: 'Basic List' },
+      DetailsList: {
+        subPages: {
+          // Names, titles, and URLs here are partial.
+          // A simple entry like this:
+          //   Basic: {}
+          // will produce an actual page like this:
+          //   component: DetailsListBasicPage
+          //   title:     DetailsList - Basic
+          //   url:       #/controls/web/detailslist/basic
+          // An entry like this:
+          //   CustomColumns: { title: 'Custom Item Columns', url: 'customitemcolumns' }
+          // will produce an actual page like this:
+          //   component: DetailsListCustomColumnsPage
+          //   title:     DetailsList - Custom Item Columns
+          //   url:       #/controls/web/detailslist/customitemcolumns
+          Basic: {},
+          Compact: {},
+          Grouped: {},
+          LargeGrouped: { title: 'Large Grouped' },
+          CustomColumns: { title: 'Custom Item Columns', url: 'customitemcolumns' },
+          CustomRows: { title: 'Custom Item Rows', url: 'customitemrows' },
+          CustomFooter: { title: 'Custom Footer' },
+          CustomGroupHeaders: { title: 'Custom Group Headers' },
+          Advanced: { title: 'Variable Row Heights', url: 'variablerowheights' },
+          DragDrop: { title: 'Drag & Drop', url: 'draganddrop' },
+          NavigatingFocus: { title: 'Inner Navigation', url: 'innernavigation' },
+          Shimmer: {}
+        }
+      },
+      GroupedList: {},
+      ActivityItem: {},
+      DocumentCard: {},
+      Facepile: {},
+      HoverCard: {},
+      Persona: {}
+    },
+    'Commands, Menus & Navs': {
+      Breadcrumb: {},
+      CommandBar: {},
+      ContextualMenu: {},
+      Nav: {},
+      OverflowSet: {},
+      Pivot: {}
+    },
+    'Notification & Engagement': {
+      Coachmark: {},
+      MessageBar: {},
+      TeachingBubble: {}
+    },
+    Progress: {
+      ProgressIndicator: {},
+      Shimmer: {},
+      Spinner: {}
+    },
+    Surfaces: {
+      Callout: {},
+      Dialog: {},
+      Modal: {},
+      Panel: {},
+      ScrollablePane: {},
+      Tooltip: {}
+    },
+    Utilities: {
+      Announced: {
+        subPages: {
+          QuickActions: { title: 'Quick Actions' },
+          SearchResults: { title: 'Search Results' },
+          LazyLoading: { title: 'Lazy Loading' },
+          BulkOperations: { title: 'Bulk Operations' }
+        }
+      },
+      FocusTrapZone: {},
+      FocusZone: {},
+      Icon: {},
+      Image: {},
+      Keytips: {},
+      Layer: {},
+      MarqueeSelection: {},
+      Overlay: {},
+      ResizeGroup: {},
+      Selection: {},
+      Separator: {},
+      Stack: {},
+      Text: {},
+      Themes: {}
+    },
+    'Fluent Theme': {
+      FluentTheme: { title: 'Fluent Theme', url: 'fluent-theme' }
+    },
+    References: {}
+    // The "Other" category can be useful for local development, but it currently can also cause
+    // non-web controls (such as Chip) to show up on the web controls page.
+    // Other: {}
+  };
 
 interface ITocConfig {
   items: Array<IYamlTocItem & { extended?: boolean }>;
@@ -12,7 +132,7 @@ interface ITocConfig {
  * In the future we can make modifications to the logic so that to include additional things
  * like pages with examples that have nothing to do with the API json files.
  */
-function generateConfig(categoriesSource: ICategory) {
+function generateConfig(categoriesSource: any) {
   const tocConfig: ITocConfig = {
     items: [
       {
@@ -50,13 +170,13 @@ function generateConfig(categoriesSource: ICategory) {
       }
     }
 
-    if (tocConfig.items[1].items) {
+    if (tocConfig.items[1] && tocConfig.items[1].items) {
       tocConfig.items[1].items.push(configItem);
     }
   }
 
   const config = {
-    tocConfig,
+    tableOfContents: tocConfig,
     catchAllCategory: 'References',
     noDuplicateEntries: true,
     // some possible filters to fill the leaf nodes `items` arrays.
@@ -65,8 +185,7 @@ function generateConfig(categoriesSource: ICategory) {
   };
 
   // writing file
-  const json = JSON.stringify(config);
-  fs.writeFileSync('config/api-documenter.json', json, 'utf8');
+  JsonFile.save(config, '../config/api-documenter.json');
 }
 
 generateConfig(categories);
