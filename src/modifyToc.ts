5f5acc6ab60d7de39faab9c065051c9087e57cea
VitalieBraga <vibraga@microsoft.com>
2019-08-07 23:45:31 -0700

    Sorted out the line injection

--- src/modifyToc.ts
+++ src/modifyToc.ts
@@ -1,6 +1,5 @@
 import * as path from 'path';
 
-import { FileSystem } from '@microsoft/node-core-library';
 import { readFile, writeFile } from './utilities';
 
 const [nodePath, scriptPath, filePath]: Array<string | undefined> = process.argv;
@@ -16,15 +15,13 @@ if (!path.isAbsolute(filePath)) {
 const tocFilePath = path.resolve(filePath, 'toc.yml');
 
 const tocFile = readFile(tocFilePath);
-console.log(tocFile);
 
 function addNewLine(match: string, offset: number, originalString: string): string {
-  return match + '    expanded: true\n';
+  return match + '\n    expanded: true';
 }
 
-const newTocFile: string = tocFile.replace('    uid: office-ui-fabric-react\n', addNewLine);
-console.log('newTocFile: ', newTocFile);
+const newTocFile: string = tocFile.replace('uid: office-ui-fabric-react', addNewLine);
 //   .replace(/items:\n/, '\n')
 //   .replace(/\n\s\s/g, '\n');
 
-FileSystem.writeFile(tocFilePath, newTocFile);
+writeFile(tocFilePath, newTocFile);
