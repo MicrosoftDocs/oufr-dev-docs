a82af6f8e9a4a7d6576816d8e893d59dea63852b
VitalieBraga <vibraga@microsoft.com>
2019-08-29 19:30:39 -0700

    Add some comments and put the yaml pipeline file in prettier ignore file

--- src/downloadApiFiles.ts
+++ src/downloadApiFiles.ts
@@ -6,24 +6,30 @@ import { parseString } from 'xml2js';
 const [nodePath, scriptPath, filesOutputPath, env]: Array<string | undefined> = process.argv;
 
 if (!filesOutputPath) {
-  throw new Error('Must specify a docs output path as the first argument to this script');
+  throw new Error('Must specify api.json files output path as the first argument to this script');
 }
 
 if (!path.isAbsolute(filesOutputPath) && env !== 'local') {
-  throw new Error('The docs output path must be absolute');
+  throw new Error('The api.json files output path must be absolute');
 }
 
 const outputDirectory = path.resolve(filesOutputPath);
 
+// urls for every api.json file to fetch
 const fileUrlsList: string[] = [];
+// helper objects containing the name and the name with the file extension of the api.json files
 const fileNamesList: Array<{ nameWithExtension: string; name: string }> = [];
 
+// Fetching a list of blob containers in xml format.
+// If this is not working a possible cause could be because Fabric team changed the blob container name or stopped uploading the files.
 fetch('https://fabricweb.blob.core.windows.net/fabric?restype=container&comp=list&prefix=api-json')
   .then(checkStatus)
-  .then(res => res.text())
+  .then(res => res.text()) // transform the xml to a string
   .then(body => {
+    // an array of fetch promises for every api.json file
     const fetchPromises: Array<Promise<Response>> = [];
 
+    // body here is a string of the xml response that will be return the result being a json object representation of it.
     parseString(body, (err, result) => {
       try {
         result.EnumerationResults.Blobs[0].Blob.forEach((file: any) => {
@@ -43,18 +49,21 @@ fetch('https://fabricweb.blob.core.windows.net/fabric?restype=container&comp=lis
       }
     });
 
+    // returning all api.json files fetch promises
     return Promise.all(fetchPromises);
   })
   .then(responses => {
     const responseToJsonPromises: Array<Promise<any>> = [];
 
     responses.forEach(response => {
+      // building an array of promises that transform the responses after fetching all the files to json objects to be written on disk.
       responseToJsonPromises.push(response.json());
     });
 
     return Promise.all(responseToJsonPromises);
   })
   .then(jsonObjects => {
+    // take every json object and along with some helper info extracted ahead write the json files on disk.
     jsonObjects.forEach(jsonObject => {
       for (let i = 0, l = fileNamesList.length; i < l; i++) {
         const file = fileNamesList[i];
@@ -75,8 +84,9 @@ fetch('https://fabricweb.blob.core.windows.net/fabric?restype=container&comp=lis
   });
 
 function checkStatus(res: Response) {
+  // res.status >= 200 && res.status < 300
   if (res.ok) {
-    // res.status >= 200 && res.status < 300
+    console.log('Successfully fetched the blob container files list');
     return res;
   } else {
     throw new Error(res.statusText);
